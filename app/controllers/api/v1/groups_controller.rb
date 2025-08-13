0b0da9cfbc047bbbcdf3443c274a1677d562245e
Murilo Cavalcante Lima <muricav@hotmail.com>
2025-08-13 03:05:59 -0300

    add rspec gem

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,49 @@
+#frozen_string_literal: true
+
+class Api::V1::GroupsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_group, only: [:show, :update, :destroy]
+    
+    def index
+        @groups = current_user.groups
+        render json: @groups, status: :ok
+    end
+    
+    def show
+        render json: @group, status: :ok
+    end
+    
+    def create
+        @group = Group.new(group_params)
+        if @group.save
+            render json: @group, status: :created
+        else
+            render json: { errors: @group.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    def update
+        if @group.update(group_params)
+        render json: @group, status: :ok
+        else
+        render json: { errors: @group.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @group.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def set_group
+        @group = current_user.groups.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Group not found' }, status: :not_found
+    end
+    
+    def group_params
+        params.require(:group).permit(:name, :description).merge(user: current_user)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include Devise modules for both Devise and Devise Token Auth
-  extend Devise::Models # <-- Adicione isso se necessário
+  extend Devise::Models
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
